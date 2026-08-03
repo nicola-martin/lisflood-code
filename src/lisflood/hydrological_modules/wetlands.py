a8b908fe47bfd6ddaab752bcff57a516d2b12294
Nicola Martin <nicola.martin@ecmwf.int>
2026-08-03 09:57:36 +0000

    Fix for wetlands doy used in restarts.

--- src/lisflood/hydrological_modules/wetlands.py
+++ src/lisflood/hydrological_modules/wetlands.py
@@ -175,7 +175,7 @@ class wetlands(HydroModule):
             if option['reservoir_lakes_Excel']:
                 # if wetlands are stored in Excel file
                 self.var.wetland_area = self.wetland_readarea(self.var.xl_settings_file_path)
-                doy = int(datetime.datetime.strptime(binding['CalendarDayStart'], '%d/%m/%Y %H:%M').strftime('%j'))
+                doy = int(datetime.datetime.strptime(binding['StepStart'], '%d-%m-%Y %H:%M').strftime('%j'))
                 self.var.WetlandAreaCC = self.var.wetland_area[doy-1,:] * 1000000
                 # back to wetlandArea , because it is used in routing_kinematic
                 self.var.wetlandArea = maskinfo.in_zero()
